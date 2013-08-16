cec4db01a35d6113da985503107ea998475f72ce
Michael Crismali <michael.crismali@gmail.com>
2013-08-16 09:36:52 -0500

    added Matcher#parse and specs

--- lib/cocaine/matcher.rb
+++ lib/cocaine/matcher.rb
@@ -1,5 +1,9 @@
 module Cocaine::Matcher
 
-
+  def self.parse(string, regexp)
+    matches = string.match regexp
+    return nil unless matches
+    Hash[matches.names.zip(matches.captures)]
+  end
 
 end
--- spec/matcher_spec.rb
+++ spec/matcher_spec.rb
@@ -4,8 +4,30 @@ describe Cocaine::Matcher do
 
   let(:matcher) { Cocaine::Matcher }
 
-  it "captures the method name" do
+  describe "#parse" do
 
+    let(:string) { "Hello, how are you?" }
+    let(:regexp) { /(?<greeting>\w+),\s(?<question>.+)(?<question_mark>\?)/ }
+
+
+    it "takes a regexp and string and returns a hash of the named matches" do
+      result = matcher.parse string, regexp
+      expected = {
+        "greeting" => "Hello",
+        "question" => "how are you",
+        "question_mark" => "?"
+      }
+      expect(result).to eq(expected)
+    end
+
+    context "when the string doesn't fully match" do
+      let(:bad_string) { "Hello, I'm well." }
+
+      it "returns nil" do
+        result = matcher.parse bad_string, regexp
+        expect(result).to be_nil
+      end
+    end
   end
 
 end
