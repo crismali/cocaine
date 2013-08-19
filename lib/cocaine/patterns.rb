5914ec28db9943f9d88b9ecde163847232f9cd79
Michael Crismali <michael.crismali@gmail.com>
2013-08-18 21:38:47 -0500

    added class/module pattern and specs

--- lib/cocaine/patterns.rb
+++ lib/cocaine/patterns.rb
@@ -1,9 +1,21 @@
 module Cocaine::Patterns
 
-  METHOD_DEF = /def \s
+  METHOD_DEF = /\s* def \s*
     (?<singleton>self\.)?
     (?<method_name>[\w\?]+)
-    \(? \s* (?<args_list>[\w\s,]*) \s* \)?
-    \n /x
+    \s* \(? \s*
+      (?<args_list>[^\(\)\;\n]*)
+    \s* \)? \s*
+    [\n\;]
+  /x
+
+  CLASS_MODULE_DEF = / \s*
+    (class|module) \s+
+    (?<class_module>\w+)
+    \s* <? \s*
+    (?<super_class_name>\w*)
+    \s*
+    [\n\;]
+  /x
 
 end
--- spec/patterns_spec.rb
+++ spec/patterns_spec.rb
@@ -19,6 +19,18 @@ describe Cocaine::Patterns do
       expect(result_2["method_name"]).to eq("my_other_method")
     end
 
+    it "works when a semicolon is used at the end instead of a new line" do
+      method = "def method;"
+      result = method.match pattern
+      expect(result).to_not be_nil
+    end
+
+    it "works when there are tons of unnecessary spaces" do
+      method = "    def     method  (  arg_1   ,   arg_2  =  5       , *args )  \n"
+      result = method.match pattern
+      expect(result).to_not be_nil
+    end
+
     context "when the method name has a number in it" do
       it "captures the method name" do
         number_method = "def method_1\n"
@@ -65,6 +77,18 @@ describe Cocaine::Patterns do
         result = method.match pattern
         expect(result["args_list"]).to eq("arg")
       end
+
+      it "captures the argument list when there's a splat" do
+        method = "def method(*args)\n"
+        result = method.match pattern
+        expect(result["args_list"]).to eq("*args")
+      end
+
+      it "captures the argument list when there's a default argument" do
+        method = "def method(arg = 5)\n"
+        result = method.match pattern
+        expect(result["args_list"]).to eq("arg = 5")
+      end
     end
 
     context "when the method is a singleton" do
@@ -82,6 +106,51 @@ describe Cocaine::Patterns do
         expect(result).to_not be_nil
         expect(result["args_list"]).to eq("arg_1")
         expect(result["singleton"]).to eq("self.")
+      end
+    end
+  end
+
+  describe "CLASS_MODULE_DEF" do
+
+    let(:pattern) { Cocaine::Patterns::CLASS_MODULE_DEF }
+
+    it "captures the class name" do
+      class_def = "class Dog\n"
+      result = class_def.match pattern
+      expect(result["class_module"]).to eq("Dog")
+    end
+
+    context "when there's inheritance" do
+      it "captures the class name" do
+       class_def = "class Dog < Animal\n"
+       result = class_def.match pattern
+       expect(result["class_module"]).to eq("Dog")
+      end
+
+      it "captures the super class' name" do
+        class_def = "class Dog < Animal\n"
+        result = class_def.match pattern
+        expect(result["super_class_name"]).to eq("Animal")
+      end
+
+      it "works when there are a ton of spaces" do
+        class_def = "    class     Dog    <   Animal     \n"
+        result = class_def.match pattern
+        expect(result).to_not be_nil
+      end
+
+      it "works when a semicolon is used at the end instead of a new line" do
+        class_def = "class Dog < Animal;"
+        result = class_def.match pattern
+        expect(result).to_not be_nil
+      end
+    end
+
+    context "when it's a module" do
+      it "captures the module name" do
+        module_def = "module Walkable\n"
+        result = module_def.match pattern
+        expect(result).to_not be_nil
       end
     end
   end
