fabef6d054721aece76c09b6ff307be30924cb3a
Michael Crismali <michael.crismali@gmail.com>
2013-09-26 08:37:47 -0500

    added replace string literals method and specs to sanitizer, reworked some specs

--- lib/cocaine/sanitizer.rb
+++ lib/cocaine/sanitizer.rb
@@ -1,19 +1,34 @@
 class Cocaine::Sanitizer
 
-  attr_accessor :removed_strings
+  attr_accessor :removed_double_quote_strings,
+    :removed_single_quote_strings
+
+
+  SINGLE_REPLACEMENT_CHAR = "COCAINE_SINGLE_REPLACEMENT_CHAR"
+  DOUBLE_REPLACEMENT_CHAR = "COCAINE_DOUBLE_REPLACEMENT_CHAR"
 
   ESCAPED_QUOTES_MAP = {
-    Cocaine::Patterns::ESCAPED_SINGLE_QUOTE => "COCAINE_SINGLE_QUOTE_STR",
-    Cocaine::Patterns::ESCAPED_DOUBLE_QUOTE => "COCAINE_DOUBLE_QUOTE_STR"
+    Cocaine::Patterns::ESCAPED_SINGLE_QUOTE => SINGLE_REPLACEMENT_CHAR,
+    Cocaine::Patterns::ESCAPED_DOUBLE_QUOTE => DOUBLE_REPLACEMENT_CHAR
   }
 
+  SINGLE_REPLACEMENT_STRING = "COCAINE_SINGLE_REPLACEMENT_STRING_"
+  DOUBLE_REPLACEMENT_STRING = "COCAINE_DOUBLE_REPLACEMENT_STRING_"
+
   STRING_LITERAL_MAP = {
-    Cocaine::Patterns::SINGLE_QUOTES_STRING => "COCAINE_SINGLE_STRING_coke",
-    Cocaine::Patterns::DOUBLE_QUOTES_STRING => "COCAINE_DOUBLE_STRING_coke"
+    single: {
+      pattern: Cocaine::Patterns::SINGLE_QUOTES_STRING,
+      replacement: SINGLE_REPLACEMENT_STRING
+    },
+    double: {
+      pattern: Cocaine::Patterns::DOUBLE_QUOTES_STRING,
+      replacement: DOUBLE_REPLACEMENT_STRING
+    }
   }
 
   def initialize
-    self.removed_strings = []
+    self.removed_double_quote_strings = []
+    self.removed_single_quote_strings = []
   end
 
   def replace_escaped_quotes(text)
@@ -25,16 +40,24 @@ class Cocaine::Sanitizer
     text_clone
   end
 
-  # def replace_string_literals(text)
-  #   quotes = []
-  #   STRING_LITERAL_MAP.each do |quote, replacement_template|
-  #     match = text.match quote
-  #     while match
-  #       index = quotes.size
-  #       quotes << match["string"]
-  #       text.sub!(quote, "#{replacement_template}#{index}")
-  #     end
-  #   end
-  # end
+  def replace_string_literals(text)
+    text_clone = text.clone
+
+    STRING_LITERAL_MAP.each do |type, info|
+      replace_type_of_literal(info.merge(type: type, text: text_clone))
+    end
+    text_clone
+  end
+
+  private
+
+  def replace_type_of_literal(options)
+    while match = options[:text].match(options[:pattern])
+      removed_strings = send("removed_#{options[:type]}_quote_strings")
+      index = removed_strings.size
+      removed_strings << match["string"]
+      options[:text].sub!(options[:pattern], "#{options[:replacement]}#{index}")
+    end
+  end
 
 end
--- spec/sanitzer_spec.rb
+++ spec/sanitzer_spec.rb
@@ -3,17 +3,30 @@ require_relative "./spec_helper"
 describe Cocaine::Sanitizer do
 
   let(:sanitizer) { Cocaine::Sanitizer.new }
+  let(:temp_single_quote_char) { Cocaine::Sanitizer::SINGLE_REPLACEMENT_CHAR }
+  let(:temp_double_quote_char) { Cocaine::Sanitizer::DOUBLE_REPLACEMENT_CHAR }
+  let(:temp_single_quote) { Cocaine::Sanitizer::SINGLE_REPLACEMENT_STRING }
+  let(:temp_double_quote) { Cocaine::Sanitizer::DOUBLE_REPLACEMENT_STRING }
 
   context "attr_accessor" do
-    it "responds to 'removed_strings'" do
-      expect(sanitizer).to respond_to :removed_strings
-      expect(sanitizer).to respond_to :removed_strings=
+    it "responds to 'removed_single_quote_strings'" do
+      expect(sanitizer).to respond_to :removed_single_quote_strings
+      expect(sanitizer).to respond_to :removed_single_quote_strings=
+    end
+
+    it "responds to 'removed_double_quote_strings'" do
+      expect(sanitizer).to respond_to :removed_double_quote_strings
+      expect(sanitizer).to respond_to :removed_double_quote_strings=
     end
   end
 
   describe "#initialize" do
-    it "sets removed_strings to be an empty array" do
-      expect(sanitizer.removed_strings).to eq([])
+    it "sets removed_double_quote_strings to be an empty array" do
+      expect(sanitizer.removed_double_quote_strings).to eq([])
+    end
+
+    it "sets removed_single_quote_strings to be an empty array" do
+      expect(sanitizer.removed_single_quote_strings).to eq([])
     end
   end
 
@@ -21,14 +34,14 @@ describe Cocaine::Sanitizer do
     it "replaces all escaped single quote marks with unlikely string" do
       string = %|some.code('words \\'go here\\' sir') \n 'Here\\'s'.more_code|
       result = sanitizer.replace_escaped_quotes(string)
-      expected = %|some.code('words COCAINE_SINGLE_QUOTE_STRgo hereCOCAINE_SINGLE_QUOTE_STR sir') \n 'HereCOCAINE_SINGLE_QUOTE_STRs'.more_code|
+      expected = %|some.code('words #{temp_single_quote_char}go here#{temp_single_quote_char} sir') \n 'Here#{temp_single_quote_char}s'.more_code|
       expect(result).to eq(expected)
     end
 
     it "replaces all escaped double quote marks with unlikely string" do
       string = %|some.code("words \\"go here\\" sir") \n "Here\\'s".more_code|
       result = sanitizer.replace_escaped_quotes(string)
-      expected = %|some.code("words COCAINE_DOUBLE_QUOTE_STRgo hereCOCAINE_DOUBLE_QUOTE_STR sir") \n "HereCOCAINE_SINGLE_QUOTE_STRs".more_code|
+      expected = %|some.code("words #{temp_double_quote_char}go here#{temp_double_quote_char} sir") \n "Here#{temp_single_quote_char}s".more_code|
       expect(result).to eq(expected)
     end
 
@@ -42,26 +55,94 @@ describe Cocaine::Sanitizer do
 
   describe "#replace_string_literals" do
 
+
+    it "doesn't change the original string" do
+      original_string = %|some.code("words go here sir")|
+      string_clone = original_string.clone
+      sanitizer.replace_string_literals(original_string)
+      expect(original_string).to eq(string_clone)
+    end
+
     context "when replacing single quote literals" do
-      xit "replaces string literals with unlikely string and index" do
-        string = %| chunk.of('code is there')|
-        result = sanitizer.replace_string_literals(string)
-        expected = %| chunk.of(COCAINE_SINGLE_STRING_coke0)|
+
+      let(:first_string_literal) { %|'code is there'| }
+      let(:second_string_literal) { %|'and strings are here'| }
+      let(:string) { %| chunk.of(#{first_string_literal}, #{second_string_literal})| }
+
+      let!(:result) { sanitizer.replace_string_literals(string) }
+
+      it "replaces string literals with unlikely string and index" do
+        expected = %| chunk.of(#{temp_single_quote}0, #{temp_single_quote}1)|
         expect(result).to eq(expected)
       end
 
+      it "adds the extracted strings to the single quote strings array" do
+        expect(sanitizer.removed_single_quote_strings).to include first_string_literal
+        expect(sanitizer.removed_single_quote_strings).to include second_string_literal
+      end
 
+      it "ensures that each string added to the arrays is at the proper index" do
+        removed_strings = sanitizer.removed_single_quote_strings
+        expect(removed_strings.first).to eq(first_string_literal)
+        expect(removed_strings.last).to eq(second_string_literal)
+      end
+
+      it "ensures that each temp string has the right index attached to it" do
+        expect(result).to match /#{temp_single_quote}0/
+        expect(result).to match /#{temp_single_quote}1/
+      end
     end
 
     context "when replacing double quote literals" do
-      xit "replaces single quote string literals with unlikely string and index" do
-        string = %| chunk.of("code is there")|
-        result = sanitizer.replace_string_literals(string)
-        expected = %| chunk.of(COCAINE_DOUBLE_STRING_coke0)|
+      let(:first_string_literal) { %|"code is there"| }
+      let(:second_string_literal) { %|"and strings are here"| }
+      let(:string) { %| chunk.of(#{first_string_literal}, #{second_string_literal})| }
+
+      let!(:result) { sanitizer.replace_string_literals(string) }
+
+      it "replaces string literals with unlikely string and index" do
+        expected = %| chunk.of(#{temp_double_quote}0, #{temp_double_quote}1)|
         expect(result).to eq(expected)
       end
 
+      it "adds the extracted strings to the double quote strings array" do
+        expect(sanitizer.removed_double_quote_strings).to include first_string_literal
+        expect(sanitizer.removed_double_quote_strings).to include second_string_literal
+      end
 
+      it "ensures that each string added to the arrays is at the proper index" do
+        removed_strings = sanitizer.removed_double_quote_strings
+        expect(removed_strings.first).to eq(first_string_literal)
+        expect(removed_strings.last).to eq(second_string_literal)
+      end
+
+      it "ensures that each temp string has the right index attached to it" do
+        expect(result).to match /#{temp_double_quote}0/
+        expect(result).to match /#{temp_double_quote}1/
+      end
+    end
+
+    context "when replacing double and single quote strings" do
+      let(:first_string_literal) { %|"code is there"| }
+      let(:second_string_literal) { %|'and strings are here'| }
+      let(:string) { %| chunk.of(#{first_string_literal}, #{second_string_literal})| }
+
+      let!(:result) { sanitizer.replace_string_literals(string) }
+
+      it "replaces string literals with unlikely string and index" do
+        expected = %| chunk.of(#{temp_double_quote}0, #{temp_single_quote}0)|
+        expect(result).to eq(expected)
+      end
+
+      it "adds the extracted strings to the double quote strings array" do
+        expect(sanitizer.removed_double_quote_strings).to include first_string_literal
+        expect(sanitizer.removed_single_quote_strings).to include second_string_literal
+      end
+
+      it "ensures that each temp string has the right index attached to it" do
+        expect(result).to match /#{temp_double_quote}0/
+        expect(result).to match /#{temp_single_quote}0/
+      end
     end
   end
 end
